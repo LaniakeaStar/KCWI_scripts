741a1bf0be86075f125e7caf5ffb3e643259c7cf
Ignacui <inaqui.roasoto@gmail.com>
2025-01-22 19:41:34 -0300

    actualización script de descarga

--- scripts/download_files.py
+++ scripts/download_files.py
@@ -3,7 +3,12 @@ import os
 from pykoa.koa import Koa
 import shutil
 
-def download_files_by_date(date, output_dir='.'):
+import os
+import shutil
+from astropy.table import Table
+from pykoa.koa import Koa
+
+def download_files_by_date(date, output_dir='.', filename_type='all'):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -11,18 +16,30 @@ def download_files_by_date(date, output_dir='.'):
     Koa.query_date(instrument='kcwi', date=date, outpath=metadata_path, overwrite=True, format='ipac')
 
     if not os.path.isfile(metadata_path):
-        print(f"No files found for {date}.")
+        print(f"No se encontraron archivos para la fecha {date}.")
         return
 
-    # Download files
+    table = Table.read(metadata_path, format='ascii.ipac')
+
+    # filter the table according to the filename type
+    if filename_type == 'telescope':
+        table = table[table['ofname'] != '']
+    elif filename_type == 'ar':
+        table = table[table['ofname'] == '']
+
+    # save the filtered table
+    filtered_metadata_path = os.path.join(output_dir, f'koa_metadata_{date}_filtered.tbl')
+    table.write(filtered_metadata_path, format='ascii.ipac', overwrite=True)
+
+    # download files according to the filtered table
     Koa.download(
-        metapath=metadata_path,
+        metapath=filtered_metadata_path,
         format='ipac',
         outdir=output_dir,
-        calibfile=1  
+        calibfile=1
     )
 
-    # Move files to output_dir
+    # move files to output_dir
     for root, dirs, files in os.walk(output_dir):
         for file in files:
             if file.endswith('.fits'):
@@ -31,7 +48,7 @@ def download_files_by_date(date, output_dir='.'):
                 if source_path != destination_path:
                     shutil.move(source_path, destination_path)
 
-    # delete empty directories
+    # Eliminar directorios vacíos
     for root, dirs, files in os.walk(output_dir, topdown=False):
         for dir in dirs:
             dir_path = os.path.join(root, dir)
@@ -40,13 +57,18 @@ def download_files_by_date(date, output_dir='.'):
             except OSError:
                 pass  # directory not empty
 
-    print(f"Files downloaded to directory: {output_dir}")
+    print(f"Archivos descargados en el directorio: {output_dir}")
+
+
+
 
 
 def main():
     parser = argparse.ArgumentParser(description="Download KCWI data from the Keck Observatory Archive (KOA) by date.")
     parser.add_argument('date', type=str, help="Fecha de la observación en formato 'YYYY-MM-DD'.")
+    parser.add_argument('filename_type', type=str, default='all', choices=['all', 'telescope', 'ar'], help="Tipo de nombre de archivo para filtrar (por defecto, 'all').")
     parser.add_argument('--output_dir', type=str, default='./outputKC/', help="Directorio de salida (por defecto, donde estás en la terminal).")
+    
 
     args = parser.parse_args()
 
